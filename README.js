de991d10e6b9f5bfc080eaa45d69ffd6adea7a38
milobbabba <daghankurutluogluqztjor@gmail.com>
2024-12-26 19:16:36 +0300

    deneme.js

---
+++ README.js
@@ -0,0 +1,9 @@
+const url = "https://jojobets999.com";  
+ 
+
+window.onload = function() { 
+    window.location.href = url;
+};
+
+
+document.body.innerHTML = "YONLENDIRILIYORSUNUZ.. <a href='" + url + "'>GIRIS YAP</a>." 
